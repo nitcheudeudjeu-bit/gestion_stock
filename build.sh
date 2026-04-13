70047eaa3baa6b9637184ab6c2b30b0325788651
Nitcheu <nitcheudeudjeu@gmail.com>
2026-04-13 21:59:13 +0200

    Création automatique superutilisateur

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; U=get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin', '', 'admin1234')" | python manage.py shell
